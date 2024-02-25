7236899545b886464a31e4b42d5aaa7b5c5fc741
gkmvit <51942997+gkmvit@users.noreply.github.com>
2024-02-25 21:50:04 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "name" {
+type=string
+description="Name for this infrastructure"
+default="adotest"
+}
+
+variable "location" {
+type=string
+description="Name of the region for this infreastructure"
+default="East US 2"
+}
